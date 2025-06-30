a930580146b208774f31836fa2e6d4d7a53c045b
OfficialNovrixProductions <jacksonflare80@gmail.com>
2025-06-30 16:58:45 +0800

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-// Random symbol picker for guard
-const symbols = ['🔺', '⬜', '⭕'];
-const randomSymbol = symbols[Math.floor(Math.random() * symbols.length)];
-document.getElementById("guard-symbol").textContent = randomSymbol;
